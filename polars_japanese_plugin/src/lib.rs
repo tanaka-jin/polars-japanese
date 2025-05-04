25e840dc11cd753ad8e910e28c1c80bf4ab0dcb0
Jin TANAKA <44251900+tanaka-jin@users.noreply.github.com>
2025-05-04 07:44:37 +0000

    feat: remove jaconv dependency and related code for character conversion

--- polars_japanese_plugin/src/lib.rs
+++ polars_japanese_plugin/src/lib.rs
@@ -5,7 +5,5 @@ mod expressions;
 #[pymodule]
 #[pyo3(name="polars_japanese_plugin")]
 fn _polars_japanese_plugin(_py: Python, _m: &Bound<'_, PyModule>) -> PyResult<()> {
-    // ここで expressions モジュール内の関数などを登録する必要があるかもしれません
-    // 例: _m.add_function(wrap_pyfunction!(expressions::some_function, _m)?)?;
     Ok(())
 }
--- src/polars_japanese/__init__.py
+++ src/polars_japanese/__init__.py
@@ -1,11 +1,9 @@
 from . import common  # noqa: F401
-from .jaconv_util import JaconvExpr
 from .japanera_util import JapaneraExpr
 from .jpholiday_util import JpholidayExpr
 from .kanjize_util import KanjizeExpr
 
 __all__ = [
-    "JaconvExpr",
     "JapaneraExpr",
     "KanjizeExpr",
     "JpholidayExpr",
